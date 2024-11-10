854a4414469b398e6b68977d79109ae3bafa849c
ag <agaudon@proton.me>
2024-11-10 17:26:01 -0330

    updated styling for mobile

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -84,7 +84,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </Head>
       <Body>
