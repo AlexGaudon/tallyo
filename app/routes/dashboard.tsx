cefd96d4b6493683ce4b18757107db2938fa2521
ag <agaudon@proton.me>
2024-11-11 12:04:38 -0330

    updated padding

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -31,7 +31,7 @@ export const Route = createFileRoute("/dashboard")({
 
 function DashboardPage() {
   return (
-    <div className="gap-y-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-2 pt-4 pb-12">
+    <div className="gap-x-4 gap-y-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-2 pt-4 pb-12">
       <div className="space-y-4">
         <CategoryBreakdownChart />
         <IncomeVsExpenseChart />
